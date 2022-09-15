714385ccbf773cb4582f6f60fe57895db192b3c6
galnir <nirnir122@gmail.com>
2022-09-15 15:08:32 +0300

    setup keystone with next

--- next.config.js
+++ next.config.js
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
+const { withKeystone } = require("@keystone-6/core/next");
+
+module.exports = withKeystone({
   reactStrictMode: true,
   swcMinify: true,
-}
-
-module.exports = nextConfig
+});
