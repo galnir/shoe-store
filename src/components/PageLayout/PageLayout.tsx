489906937e09c6c267b0012a3d3da172d6d31ecf
galnir <nirnir122@gmail.com>
2022-09-18 22:25:58 +0300

    move Head to layout component

--- src/components/PageLayout/PageLayout.tsx
+++ src/components/PageLayout/PageLayout.tsx
@@ -1,9 +1,15 @@
+import Head from "next/head";
 import React from "react";
 import Header from "../Header";
 
 const PageLayout = ({ children }: { children: React.ReactNode }) => {
   return (
     <div>
+      <Head>
+        <title>Abibas</title>
+        <meta name="description" content="Abibas, the best online shoe store" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
       <Header />
       <div className="p-8 my-0 mx-auto">{children}</div>
     </div>
