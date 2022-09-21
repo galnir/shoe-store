1c607cd0c09a8c8718cd93b6f5632896dfbadaa1
galnir <nirnir122@gmail.com>
2022-09-21 16:41:52 +0300

    redirect from /sell to /signin if user not signed in

--- src/pages/sell.tsx
+++ src/pages/sell.tsx
@@ -2,13 +2,22 @@ import { ReactElement } from "react";
 import CreateProduct from "../components/CreateProduct";
 import PageLayout from "../components/PageLayout";
 import { NextPageWithLayout } from "./_app";
+import { CURRENT_USER_QUERY } from "../components/Header";
+import { useQuery } from "@apollo/client";
+import { useRouter } from "next/router";
 
 const SellPage: NextPageWithLayout = () => {
-  return (
-    <div>
-      <CreateProduct />
-    </div>
-  );
+  const router = useRouter();
+
+  const { data, error, loading } = useQuery(CURRENT_USER_QUERY);
+  if (loading) return <p>Loading...</p>;
+  if (error) return <p>Error: {error.message}</p>;
+
+  if (!data.authenticatedItem || !data.authenticatedItem.id) {
+    router.push("/signin");
+  }
+
+  return <CreateProduct />;
 };
 
 SellPage.getLayout = function getLayout(page: ReactElement) {
