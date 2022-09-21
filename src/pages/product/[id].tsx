66f489a514ef2ae202eaf5cea812389abf1f3c8c
galnir <nirnir122@gmail.com>
2022-09-22 00:03:09 +0300

    generate product pages at build time

--- src/pages/product/[id].tsx
+++ src/pages/product/[id].tsx
@@ -1,54 +1,18 @@
 import { useRouter } from "next/router";
+import { GetStaticPathsResult, GetStaticPropsContext } from "next";
+import { query } from ".keystone/api";
 import { ReactElement } from "react";
 import PageLayout from "../../components/PageLayout";
 import { NextPageWithLayout } from "../_app";
-import { useQuery, gql } from "@apollo/client";
 import Image from "next/image";
+import type { ProductType } from "../products";
 
-const PRODUCT_QUERY = gql`
-  query GetProduct($id: ID!) {
-    product(where: { id: $id }) {
-      name
-      id
-      price
-      description
-      photo {
-        id
-        image {
-          publicUrlTransformed
-        }
-      }
-    }
-  }
-`;
-
-const ProductContent: React.FC<{ id: string }> = ({ id }) => {
-  const { loading, error, data } = useQuery(PRODUCT_QUERY, {
-    variables: { id },
-  });
-
-  if (loading) return <p>Loading...</p>;
-  if (error) return <p>{error.message}</p>;
-
-  return (
-    <div className="flex gap-14">
-      <div>
-        <Image
-          src={data?.product?.photo?.image?.publicUrlTransformed}
-          height={400}
-          width={400}
-          objectFit="cover"
-        />
-      </div>
-      <div className="font-bold">
-        <h1 className="text-3xl">{data?.product?.name}</h1>
-        <p className="text-xl">{data?.product?.description}</p>
-      </div>
-    </div>
-  );
-};
-
-const ProductPage: NextPageWithLayout = () => {
+// @ts-ignore
+const ProductPage: NextPageWithLayout = ({
+  product,
+}: {
+  product: ProductType;
+}) => {
   const { query } = useRouter();
   const { id } = query;
 
@@ -56,8 +20,52 @@ const ProductPage: NextPageWithLayout = () => {
     return <div>Invalid product id</div>;
   }
 
-  return <ProductContent id={id} />;
+  return (
+    <div className="flex gap-14">
+      <div>
+        <Image
+          src={product?.photo?.image?.publicUrlTransformed}
+          height={400}
+          width={400}
+          objectFit="cover"
+        />
+      </div>
+      <div className="font-bold">
+        <h1 className="text-3xl">{product?.name}</h1>
+        <p className="text-xl">{product?.description}</p>
+      </div>
+    </div>
+  );
 };
+
+export async function getStaticPaths(): Promise<GetStaticPathsResult> {
+  const products = (await query.Product.findMany({
+    query: "id",
+  })) as { id: string }[];
+
+  return {
+    paths: products.map((product) => ({
+      params: { id: product.id },
+    })),
+    fallback: false,
+  };
+}
+
+export async function getStaticProps({
+  params,
+}: GetStaticPropsContext<{ id: string }>) {
+  const product = (await query.Product.findOne({
+    where: { id: params?.id },
+    query:
+      "id name description price photo { id image { publicUrlTransformed } }",
+  })) as ProductType | null;
+
+  return {
+    props: {
+      product,
+    },
+  };
+}
 
 ProductPage.getLayout = function getLayout(page: ReactElement) {
   return <PageLayout>{page}</PageLayout>;
--- src/pages/products.tsx
+++ src/pages/products.tsx
@@ -1,41 +1,46 @@
 import { ReactElement } from "react";
 import PageLayout from "../components/PageLayout";
 import { NextPageWithLayout } from "./_app";
-import { useQuery, gql } from "@apollo/client";
-import { Product as ProductType } from "@prisma/client";
+import { InferGetStaticPropsType } from "next";
 import Product from "../components/Product";
+import { query } from ".keystone/api";
 
-const GET_PRODUCTS = gql`
-  query GetProducts {
-    products {
-      name
-      id
-      price
-      description
-      photo {
-        id
-        image {
-          publicUrlTransformed
-        }
-      }
-    }
-  }
-`;
+export type ProductType = {
+  id: string;
+  name: string;
+  price: number;
+  description: string;
+  photo: {
+    image: {
+      publicUrlTransformed: string;
+    };
+  };
+};
 
-const ProductsPage: NextPageWithLayout = () => {
-  const { loading, error, data } = useQuery(GET_PRODUCTS);
-
-  if (loading) return <p>Loading...</p>;
-  if (error) return <p>{error.message}</p>;
-
+// @ts-ignore
+const ProductsPage: NextPageWithLayout = ({
+  products,
+}: InferGetStaticPropsType<typeof getStaticProps>) => {
   return (
     <div className="flex flex-wrap justify-center items-center gap-8 py-18">
-      {data?.products?.map((product: ProductType) => (
+      {products?.map((product: ProductType) => (
         <Product key={product.id} product={product} />
       ))}
     </div>
   );
 };
+
+export async function getStaticProps() {
+  const products = (await query.Product.findMany({
+    query: "id name description price",
+  })) as ProductType[];
+
+  return {
+    props: {
+      products,
+    },
+  };
+}
 
 ProductsPage.getLayout = function getLayout(page: ReactElement) {
   return <PageLayout>{page}</PageLayout>;
