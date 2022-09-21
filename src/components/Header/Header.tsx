11cb97104da87fc0b67f58c3fc79459a03515c33
galnir <nirnir122@gmail.com>
2022-09-21 21:43:08 +0300

    hide sell, orders and account links if user not signed in

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,5 +1,5 @@
 import Link from "next/link";
-import { gql } from "@apollo/client";
+import { gql, useQuery } from "@apollo/client";
 import Logo from "../Logo";
 
 export const CURRENT_USER_QUERY = gql`
@@ -15,6 +15,10 @@ export const CURRENT_USER_QUERY = gql`
 `;
 
 const Header = () => {
+  const { data, error, loading } = useQuery(CURRENT_USER_QUERY);
+  if (loading) return <p>Loading...</p>;
+  if (error) return <p>Error: {error.message}</p>;
+
   return (
     <div className="p-6 flex items-center justify-between border-b-[10px] border-gray-400">
       <div>
@@ -22,9 +26,15 @@ const Header = () => {
       </div>
       <ul className="text-3xl font-bold uppercase flex gap-10">
         <Link href="/products">Products</Link>
-        <Link href="/sell">Sell</Link>
-        <Link href="/orders">Orders</Link>
-        <Link href="/account">Account</Link>
+        {data.authenticatedItem && data.authenticatedItem.id ? (
+          <>
+            <Link href="/sell">Sell</Link>
+            <Link href="/orders">Orders</Link>
+            <Link href="/account">Account</Link>
+          </>
+        ) : (
+          <Link href="/signin">Sign In</Link>
+        )}
       </ul>
     </div>
   );
